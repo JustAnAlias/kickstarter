71f21ba5a0f070fb99559277233f037333b40d89
szEIgo <cph-js226@cphbusiness.dk>
2017-05-16 16:29:33 +0200

    working classifiers

--- classifier.py
+++ classifier.py
@@ -9,7 +9,7 @@ def classifyFromUrl(url):
 
 def classifyFromPath(path):
         visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
-        print(json.dumps(visual_recognition.classify(images_file=path), indent=2))
+        print(json.dumps(visual_recognition.classify(images_file=open(path,"rb")), indent=2))
 
 
 #classifyFromUrl('https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg')
