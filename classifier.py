35894482d2af0e4b1d97666b06a77e97a9065820
szEIgo <cph-js226@cphbusiness.dk>
2017-05-16 12:54:31 +0200

    extractor

--- classifier.py
+++ classifier.py
@@ -5,7 +5,7 @@ from watson_developer_cloud import VisualRecognitionV3
 test_url = 'https://www.ibm.com/ibm/ginni/images' \
            '/ginni_bio_780x981_v4_03162016.jpg'
 
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='1UUq8kmpXJyD_n4Kn_CaQP0-tSQg1zvnNI1eVUlnDBSa')
+visual_recognition = VisualRecognitionV3('2017-05-16', api_key='1UUq8kmpXJyD_n4Kn_CaQP0-tSQg1zvnNI1eVUlnDBSa')
 
 
 print(json.dumps(visual_recognition.classify(images_url=test_url), indent=2))
