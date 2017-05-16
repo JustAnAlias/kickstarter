27140a30b6d9789649675f54e53a5639197d86b7
michael <michael.1978@gmail.com>
2017-05-16 16:18:54 +0200

    uden billeder  - har tilføjet *.jpg i gitignore

--- classifier.py
+++ classifier.py
@@ -2,16 +2,15 @@ import json
 from os.path import join, dirname
 from watson_developer_cloud import VisualRecognitionV3
 
-test_url = 'https://www.ibm.com/ibm/ginni/images' \
-           '/ginni_bio_780x981_v4_03162016.jpg'
+
+def classifyFromUrl(url):
+        visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
+        print(json.dumps(visual_recognition.classify(images_url=url), indent=2))
+
+def classifyFromPath(path):
+        visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
+        print(json.dumps(visual_recognition.classify(images_file=path), indent=2))
 
 
-#  2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649
-
-# visual_recognition = VisualRecognitionV3('2017-05-16', api_key='1UUq8kmpXJyD_n4Kn_CaQP0-tSQg1zvnNI1eVUlnDBSa')
-#>>>>>>> 35894482d2af0e4b1d97666b06a77e97a9065820
-
-# visual_recognition = VisualRecognitionV3('2017-05-16', api_key='1UUq8kmpXJyD_n4Kn_CaQP0-tSQg1zvnNI1eVUlnDBSa')
-visual_recognition = VisualRecognitionV3('2017-05-16', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
-
-print(json.dumps(visual_recognition.classify(images_url=test_url), indent=2))
+#classifyFromUrl('https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg')
+classifyFromPath('./bears-vs-babies-a-card-game-1-base/bears-vs-babies-a-card-game-1-base0.jpg')
---
+++ video2frames.py
@@ -0,0 +1,21 @@
+import os
+import cv2
+
+def makeFrames(videofile):
+        videoname = videofile[:-4]
+        folder = videoname
+        os.mkdir(folder)
+
+        vidcap = cv2.VideoCapture(videofile)
+        count = 0
+        while True:
+                success,image = vidcap.read()
+                if not success:
+                        break
+                cv2.imwrite(os.path.join(folder,videoname+"{:d}.jpg".format(count)), image)
+                count += 1
+                if(count % 100 == 0):
+                        print(count, "frames extracted")
+        print("{} images are extracted in {}.".format(count,folder))
+
+makeFrames('bears-vs-babies-a-card-game-1-base.mp4')
