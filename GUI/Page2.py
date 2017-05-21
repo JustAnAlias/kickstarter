88da9b38aae96b4c0cba36abeb57519cb17b29d2
JustAnAlias <justanalias>
2017-05-21 21:42:49 +0200

    GUI Page1 and 2 start

---
+++ GUI/Page2.py
@@ -0,0 +1,120 @@
+import sys
+
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+
+try:
+    import ttk
+    py3 = 0
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = 1
+
+def vp_start_gui():
+    '''Starting point when module is the main routine.'''
+    global val, w, root
+    root = Tk()
+    top = New_Toplevel_1 (root)
+    root.mainloop()
+
+w = None
+def create_New_Toplevel_1(root, *args, **kwargs):
+    '''Starting point when module is imported by another program.'''
+    global w, w_win, rt
+    rt = root
+    w = Toplevel (root)
+    top = New_Toplevel_1 (w)
+    Page2_support.init(w, top, *args, **kwargs)
+    return (w, top)
+
+def destroy_New_Toplevel_1():
+    global w
+    w.destroy()
+    w = None
+
+
+class New_Toplevel_1:
+    def __init__(self, top=None):
+        '''This class configures and populates the toplevel window.
+           top is the toplevel containing window.'''
+        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _fgcolor = '#000000'  # X11 color: 'black'
+        _compcolor = '#d9d9d9' # X11 color: 'gray85'
+        _ana1color = '#d9d9d9' # X11 color: 'gray85' 
+        _ana2color = '#d9d9d9' # X11 color: 'gray85' 
+
+        top.geometry("600x450+1273+396")
+        top.title("Kickstarter Evaluator")
+        top.configure(background="#d9d9d9")
+
+
+
+        self.Button1 = Button(top)
+        self.Button1.place(relx=0.13, rely=0.11, height=42, width=129)
+        self.Button1.configure(activebackground="#d9d9d9")
+        self.Button1.configure(activeforeground="#000000")
+        self.Button1.configure(background="#d9d9d9")
+        self.Button1.configure(disabledforeground="#a3a3a3")
+        self.Button1.configure(foreground="#000000")
+        self.Button1.configure(highlightbackground="#d9d9d9")
+        self.Button1.configure(highlightcolor="black")
+        self.Button1.configure(pady="0")
+        self.Button1.configure(text='''Categories''')
+        self.Button1.configure(width=129)
+
+        self.Button2 = Button(top)
+        self.Button2.place(relx=0.4, rely=0.11, height=42, width=127)
+        self.Button2.configure(activebackground="#d9d9d9")
+        self.Button2.configure(activeforeground="#000000")
+        self.Button2.configure(background="#d9d9d9")
+        self.Button2.configure(disabledforeground="#a3a3a3")
+        self.Button2.configure(foreground="#000000")
+        self.Button2.configure(highlightbackground="#d9d9d9")
+        self.Button2.configure(highlightcolor="black")
+        self.Button2.configure(pady="0")
+        self.Button2.configure(text='''Projects''')
+        self.Button2.configure(width=127)
+
+        self.Button3 = Button(top)
+        self.Button3.place(relx=0.7, rely=0.11, height=42, width=134)
+        self.Button3.configure(activebackground="#d9d9d9")
+        self.Button3.configure(activeforeground="#000000")
+        self.Button3.configure(background="#d9d9d9")
+        self.Button3.configure(disabledforeground="#a3a3a3")
+        self.Button3.configure(foreground="#000000")
+        self.Button3.configure(highlightbackground="#d9d9d9")
+        self.Button3.configure(highlightcolor="black")
+        self.Button3.configure(pady="0")
+        self.Button3.configure(text='''Evaluations''')
+        self.Button3.configure(width=134)
+
+        self.Label1 = Label(top)
+        self.Label1.place(relx=0.1, rely=0.29, height=31, width=479)
+        self.Label1.configure(background="#d9d9d9")
+        self.Label1.configure(disabledforeground="#a3a3a3")
+        self.Label1.configure(foreground="#000000")
+        self.Label1.configure(text='''Pick a category to find a project to start evaluating a project''')
+
+        self.Button4 = Button(top)
+        self.Button4.place(relx=0.15, rely=0.6, height=42, width=41)
+        self.Button4.configure(activebackground="#d9d9d9")
+        self.Button4.configure(activeforeground="#000000")
+        self.Button4.configure(background="#d9d9d9")
+        self.Button4.configure(disabledforeground="#a3a3a3")
+        self.Button4.configure(foreground="#000000")
+        self.Button4.configure(highlightbackground="#d9d9d9")
+        self.Button4.configure(highlightcolor="black")
+        self.Button4.configure(pady="0")
+        self.Button4.configure(text='''Cat''')
+
+
+
+
+
+
+if __name__ == '__main__':
+    vp_start_gui()
+
+
