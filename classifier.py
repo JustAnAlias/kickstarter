f2eb2596548798e36c4868b07edb5f96bf4bc91d
szEIgo <cph-js226@cphbusiness.dk>
2017-05-24 11:19:58 +0200

    classifier.py

--- classifier.py
+++ classifier.py
@@ -1,17 +1,31 @@
 import json
-from os.path import join, dirname
+#from os.path import join, dirname
+import os
 from watson_developer_cloud import VisualRecognitionV3
 
 
-
+classifications = []
 def classifyFromUrl(url):
-        visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
-        print(json.dumps(visual_recognition.classify(images_url=url), indent=2))
+         visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
+         print(json.dumps(visual_recognition.classify(images_url=url), indent=2))
 
 def classifyFromPath(path):
-        visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
-        return(visual_recognition.classify(images_file=open(path, 'rb')))
+         visual_recognition = VisualRecognitionV3('2016-05-20', api_key='2f26c30ef3bd18e4310f163da0fdbe7a0d2a6649')
+         return(visual_recognition.classify(images_file=open(path, 'rb')))
 
 
-with open('imageClassifications.json','w') as fp:
-    json.dump(classifyFromPath("videos/bring-reading-rainbow-back-for-every-child-everywh/frames/bring-reading-rainbow-back-for-every-child-everywh-1-base0.jpg"), fp, indent=4)
+# with open('imageClassifications.json','w') as fp:
+#     json.dump(classifyFromPath("videos/bring-reading-rainbow-back-for-every-child-everywh/frames/bring-reading-rainbow-back-for-every-child-everywh-1-base0.jpg"), fp, indent=4)
+
+def allVideos(root_dir):
+        for directory, subdirectories, files in os.walk(root_dir):
+                for file in files:
+                        file2json = file[:-4] + ".json"
+                        with open(directory+file2json,'a') as fp:
+                                #print(directory)
+                                #print(path2frame)
+                                #classifications.append(classifyFromPath(path2frame))
+                                json.dump(classifyFromPath(os.path.join(directory, file)), fp, indent=4)
+        
+allVideos("videos")
+#print(classifications)
