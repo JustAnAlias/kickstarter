eb91a91cc5c4784b35a9daf98326fd6e18535f5f
szEIgo <cph-js226@cphbusiness.dk>
2017-05-24 10:44:44 +0200

    kickstarter.py

--- kickstarter.py
+++ kickstarter.py
@@ -9,21 +9,22 @@ wee={}
 print("Starting scraper")
 
 #scan through pages 1 to end_page for data, 20 results per page
-for page in range(1,end_page+1):
-    r = requests.get('https://www.kickstarter.com/discover/advanced.json?sort=most_funded&page=' + str(page))
-    data = r.json()
+def run():
+        for page in range(1,end_page+1):
+                r = requests.get('https://www.kickstarter.com/discover/advanced.json?sort=most_funded&page=' + str(page))
+                data = r.json()
 
-    for index in range(len(data["projects"])):
+        for index in range(len(data["projects"])):
 
         #Define Values for keys
-        url = ('/'.join(("https://www.kickstarter.com/projects", str(data["projects"][index]["id"])[:-1], data["projects"][index]["slug"])))
-        id = data["projects"][index]["id"]
-        name = data["projects"][index]["name"]
-        category = data["projects"][index]["category"]["name"]
+                url = ('/'.join(("https://www.kickstarter.com/projects", str(data["projects"][index]["id"])[:-1], data["projects"][index]["slug"])))
+                id = data["projects"][index]["id"]
+                name = data["prpipojects"][index]["name"]
+                category = data["projects"][index]["category"]["name"]
         #Insert into Dictionary
-        wee[data["projects"][index]["slug"]] = {"id": id, "name": name, "url": url, "category": category}
+                wee[data["projects"][index]["slug"]] = {"id": id, "name": name, "url": url, "category": category}
 
 
-with open('data.json', 'w') as fp:
-	json.dump(wee, fp, indent=4)
-print("Dumped data to data.json")
+        with open('data.json', 'w') as fp:
+	        json.dump(wee, fp, indent=4)
+        print("Dumped data to data.json")
